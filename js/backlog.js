20181963d09fd9846fa7668e7633a8160a2e4379
Dominik Niemietz <d.niemietz97@gmx.de>
2022-02-02 22:07:16 +0100

    backlog improvement

--- js/backlog.js
+++ js/backlog.js
@@ -23,7 +23,7 @@ generateBacklog = () => {
         <div id="titleTask${i}" class="taskTitle">${task.title}</div>
         <div id="dateTask${i}" class="taskDate">${task.date}</div>
     </div>
-    <div id="details${i}" onClick="openDetails(${i})" class="taskBottom">
+    <div id="details${i}" onClick="openDetails(${tasks.indexOf(task)})" class="taskBottom">
         <div class="taskDetails">
         <div id="detailsTask${i}" class="innerTaskDetails">${task.details}</div>
         </div>
@@ -35,23 +35,47 @@ generateBacklog = () => {
     })
 }
 
-openDetails = (i) => {
-    document.getElementById(`details`).classList.remove("hide");
+openDetails = (x) => {
+    let task = tasks[x];
+    clearDetails();
+    document.getElementById(`backgroundDetails`).classList = "fadeIn";
+    document.getElementById(`detailsWindow`).classList.remove("hide");
+
+    document.getElementById(`categoryInfo`).innerHTML = `<em>Category</em> - ${task.category}`
+    for (let i = 0; i < task.users.length; i++) {
+
+        document.getElementById("userDetailed").innerHTML += `
+        <div class="memberInfo">
+        <img id="detailedImg${i}" class="memberImg big"  src="${task.users[i].pic}" style="box-shadow: 1px 1px 5px 1px ${task.users[i].color}">
+        <div>
+        <span class="infoUser"><b>Name:</b> ${task.users[i].name}</span> <span class="infoUser"><b>tel.Nr:</b> ${task.users[i].phone}</span> <span class="infoUser"><b>E-mail:</b> ${task.users[i].mail}</span>
+        </div>
 
 
+        `
+    };
+
+    document.getElementById(`fullText`).innerHTML = `<p>${task.details}<p>`;
 }
 
 getUsersBacklog = (element) => {
     let userPicString = "";
     element.users.forEach((user) =>
-        userPicString += `<img class="memberImg" onmouseover="hover()" title="` + generateTitle(user) + `" src="${user.pic}" style="box-shadow: 1px 1px 5px 1px ${user.color}">`)
+        userPicString += `<img class="memberImg" title="` + generateTitle(user) + `" src="${user.pic}" style="box-shadow: 1px 1px 5px 1px ${user.color}">`)
     return userPicString;
 }
 
 
-hover = () => {
-    console.log("hallo")
+closeDetails = () => {
+
+    document.getElementById(`backgroundDetails`).classList = "fadeOut";
+    document.getElementById(`detailsWindow`).classList.add("hide");
 }
+
+clearDetails = () => {
+    document.getElementById("userDetailed").innerHTML = null;
+}
+
 
 generateTitle = (user) => {
     return `
