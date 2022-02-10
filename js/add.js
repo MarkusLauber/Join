d045316e76e786108802ae3cac097e8221975261
MarkusLauber <lauber.m@gmx.net>
2022-02-10 14:30:12 +0100

    add.js refactoring & documentation

--- js/add.js
+++ js/add.js
@@ -2,15 +2,22 @@ let userArea;
 let addedUsers;
 let currentUsers = [];
 
+
+/**
+ * Initialisation
+ */
 init = () => {
     colors();
     includeHTML();
     serverLoad();
     userArea = document.getElementById("userArea");
     addedUsers = document.getElementById("addedUsers");
-    updateUsers();
+    updateAddedUsers();
 }
 
+/**
+ * Set Inpultfields to  null / default
+ */
 clearTask = () => {
     document.getElementById("title").value = null;
     document.getElementById("category").value = "Product";
@@ -19,9 +26,12 @@ clearTask = () => {
     document.getElementById("urgency").value = "prio2";
     currentUsers = [];
     users.forEach((user) => { user.assigned = false })
-    updateUsers();
+    updateAddedUsers();
 }
 
+/**
+ * Saves Inpult to Server
+ */
 saveTask = () => {
     let task = {
         "status": "backlog",
@@ -38,28 +48,15 @@ saveTask = () => {
     clearTask();
 }
 
-updateUsers = () => {
-    updateAddedUsers();
-}
-
+/**
+ * updates User Area about assigned Users
+ */
 generateUserArea = () => {
     let screen = document.getElementById("screenContainer");
     let freeUsers = filterFreeUsers();
     let UserAreaString = `<div id="userArea" class="user_area box-shadow">`;
-    freeUsers.forEach((user, i) => {
-
-        UserAreaString += `<div class="usercard" onclick="addUser(${users.indexOf(user)})">
-                                <div class="userinfo">
-                                    <img class="userpic" src="${user.pic}" style="box-shadow: 1px 1px 5px 1px ${user.color}">
-                                    <div class="userdata">
-                                        <div class="username">${user.name}</div>
-                                        <div>
-                                            <div class="userdetail"><span class="dark">Email: </span>${user.mail}</div>
-                                            <div class="userdetail"><span class="dark">Phone: </span>${user.phone}</div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>`
+    freeUsers.forEach((user) => {
+        UserAreaString += userString(user)
     })
     UserAreaString += "</div>"
     screen.innerHTML = UserAreaString;
@@ -69,6 +66,27 @@ generateUserArea = () => {
     }, { once: true })
 }
 
+/**
+ * creates String with Userinformation
+ */
+userString = (user) => {
+    return `<div class="usercard" onclick="addUser(${users.indexOf(user)})">
+    <div class="userinfo">
+        <img class="userpic" src="${user.pic}" style="box-shadow: 1px 1px 5px 1px ${user.color}">
+        <div class="userdata">
+            <div class="username">${user.name}</div>
+            <div>
+                <div class="userdetail"><span class="dark">Email: </span>${user.mail}</div>
+                <div class="userdetail"><span class="dark">Phone: </span>${user.phone}</div>
+            </div>
+        </div>
+    </div>
+</div>`
+}
+
+/**
+ * show/updates assigned Users and Add Button
+ */
 updateAddedUsers = () => {
     addedUsers.innerHTML = null;
     currentUsers.forEach((user, i) => {
@@ -79,10 +97,16 @@ updateAddedUsers = () => {
     }
 }
 
+/**
+ * closes UserArea
+ */
 closeUserArea = () => {
     document.getElementById("screenContainer").innerHTML = null;
 }
 
+/**
+ * adds a User to Task
+ */
 addUser = (i) => {
     currentUsers.push(users[i]);
     users[i].assigned = true;
@@ -90,12 +114,18 @@ addUser = (i) => {
     updateAddedUsers();
 }
 
+/**
+ * removes a User from Task
+ */
 removeUser = (i, j) => {
     currentUsers.splice(i, 1);
     users[j].assigned = false;
     updateAddedUsers();
 }
 
+/**
+ * chechs if all fields are filled
+ */
 allFilled = () => {
     if ((document.getElementById("title").value.length == 0) ||
         (document.getElementById("details").value.length == 0) ||
@@ -105,6 +135,9 @@ allFilled = () => {
     } else { saveTask() }
 }
 
+/**
+ * shows window to fill all inputfields
+ */
 showFillscreen = () => {
     let screen = document.getElementById("screenContainer")
     screen.innerHTML = '<div class="savescreen box-shadow">Bitte füllen Sie die Eingabefelder aus.</div>'
